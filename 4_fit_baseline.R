da746a93a4ddc1de65ebde75238b188139f889e5
miracleramos2025 <miracleramos2025@u.northwestern.edu>
2025-03-07 12:23:53 -0600

    baseline / lm models

---
+++ 4_fit_baseline.R
@@ -0,0 +1,37 @@
+# STAT 301-2 Final Project
+# Fitting the baseline model
+# 4_fit_baseline.R
+
+# load packages
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# load train-test split
+load(here("data/airbnb_train.rda"))
+load(here("data/airbnb_test.rda"))
+
+# define baseline model (predicts mean price)
+baseline_model <- linear_reg() |>
+  set_engine("lm") |>
+  set_mode("regression")
+
+# fit model
+baseline_fit <- baseline_model |>
+  fit(price ~ 1, data = airbnb_train)
+
+# predict on test set
+baseline_preds <- predict(baseline_fit, new_data = airbnb_test) |>
+  bind_cols(airbnb_test)
+
+# calculate rmse
+rmse_baseline <- rmse(baseline_preds, truth = price, estimate = .pred)
+
+# save fitted model in models/
+save(baseline_fit, file = here("models/baseline_fit.rda"))
+
+# save / write out model performance 
+baseline_performance <- data.frame(Model = "Baseline", RMSE = rmse_baseline$.estimate)
+save(baseline_performance, file = here("results/model_performance.rda"))
+
+
---
+++ 4_fit_lm.R
@@ -0,0 +1,50 @@
+# STAT 301-2 Final Project
+# Fitting the linear model
+# 4_fit_lm.R
+
+# load packages
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# load train-test split
+load(here("data/airbnb_train.rda"))
+load(here("data/airbnb_test.rda"))
+
+# define basic recipe 1 (unprepped)
+recipe1_basic <- recipe(price ~ ., data = airbnb_train) |>
+  step_rm(name, host_name) |>  
+  step_other(all_nominal(), -all_outcomes(), threshold = 0.01) |>  
+  step_unknown(all_nominal(), -all_outcomes()) |>  
+  step_dummy(all_nominal(), -all_outcomes()) |>  
+  step_zv(all_predictors()) |>  
+  step_impute_median(all_numeric(), -all_outcomes()) |>  
+  step_normalize(all_numeric(), -all_outcomes())  
+
+# define linear model
+lm_model <- linear_reg() |>
+  set_engine("lm") |>
+  set_mode("regression")
+
+# create workflow
+lm_wflow <- workflow() |>
+  add_recipe(recipe1_basic) |>  # use unprepped recipe
+  add_model(lm_model)
+
+# fit model
+lm_fit <- lm_wflow |>
+  fit(data = airbnb_train)
+
+# predict on test set
+lm_preds <- predict(lm_fit, new_data = airbnb_test) |>
+  bind_cols(airbnb_test)
+
+# calculate rmse
+rmse_lm <- rmse(lm_preds, truth = price, estimate = .pred)
+
+# save / write out fitted model
+save(lm_fit, file = here("models/lm_fit.rda"))
+
+# save / write out model performance
+lm_performance <- data.frame(Model = "Linear Model", RMSE = rmse_lm$.estimate)
+save(lm_performance, file = here("results/model_performance_lm.rda"))
